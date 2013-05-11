7fcde4892e976311ed3027e6754cc70cea48085c
Martin Rabaglia <tinchoforever@gmail.com>
2013-05-11 20:01:02 -0300

    Gatitos mockeados para lugares

--- php/application/controllers/api/points.php
+++ php/application/controllers/api/points.php
@@ -14,7 +14,15 @@ class Api_Points_Controller extends Base_Controller {
 
     public function get_all()
     {
-        $result = array('data' => array());
+        $data =  array();
+        for ($i=0; $i < 10; $i++) {
+            $data[] = array(
+                "latitude"=> 45 + $i*10,
+                "longitude"=> -60 + $i*3,
+                "picture"=> "http://placekitten.com/200/300",
+                "tags" => "nices push bro");
+        }
+        $result = array($data);
         return Response::json($result);
     }
 
